20654e177edc6d599d562b74fefc80be4e40a210
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-10 14:36:01 +0100

    Embedded form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,12 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def new
@@ -26,6 +28,5 @@ class RestaurantsController < ApplicationController
   def params_restaurant
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
-
 
 end
