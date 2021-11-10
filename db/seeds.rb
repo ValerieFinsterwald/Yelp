bd6b7afe924888d72f2b2e57a16bcbadcb25d564
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-10 14:15:55 +0100

    Restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting"
+Restaurant.destroy_all
+
+puts "creating"
+
+5.times do
+  resto = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+  resto.save
+  puts "Restaurant with id #{resto.id} was created"
+end
